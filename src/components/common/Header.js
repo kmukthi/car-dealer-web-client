81c79cf50e1bb022df781454b6d3a4fd5ad3642b
kmukthi <kmukthi@gmail.com>
2020-01-01 23:21:35 +0530

    Header.js

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,21 +1,48 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { NavLink } from "react-router-dom";
+import Burger from "./menu/Burger";
+import Menu from "./menu/Menu";
 
 const Header = () => {
   const activeStyle = { color: "#FFF" };
+  const [open, setOpen] = useState(false);
+
+  const useOnClickOutside = (ref, handler) => {
+    useEffect(() => {
+      const listener = event => {
+        if (!ref.current || ref.current.contains(event.target)) {
+          return;
+        }
+        handler(event);
+      };
+      document.addEventListener("mousedown", listener);
+      return () => {
+        document.removeEventListener("mousedown", listener);
+      };
+    }, [ref, handler]);
+  };
+
+  const node = useRef();
+  useOnClickOutside(node, () => setOpen(false));
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
   return (
     <nav className="header">
-      <NavLink to="/" exact activeStyle={activeStyle}>
+      <NavLink className="left" to="/" exact activeStyle={activeStyle}>
         Home
       </NavLink>{" "}
       {" | "}
-      <NavLink to="/cars" exact activeStyle={activeStyle}>
+      <NavLink className="left" to="/cars" exact activeStyle={activeStyle}>
         Cars
       </NavLink>{" "}
       {" | "}
-      <NavLink to="/about" activeStyle={activeStyle}>
+      <NavLink className="left" to="/about" activeStyle={activeStyle}>
         About
       </NavLink>
+      <Burger node={node} open={open} onClick={handleClick} />
+      <Menu open={open} />
     </nav>
   );
 };
