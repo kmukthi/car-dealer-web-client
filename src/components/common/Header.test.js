14239baa39a391dfc4b316b0297628dc423a6fd8
kmukthi <kmukthi@gmail.com>
2020-01-02 14:08:56 +0530

    header

--- src/components/common/Header.test.js
+++ src/components/common/Header.test.js
@@ -2,7 +2,6 @@ import React from "react";
 import Header from "./Header";
 import { shallow } from "enzyme";
 
-// Note how with shallow render you search for the React component tag
 it("contains 3 NavLinks via shallow", () => {
   const numLinks = shallow(<Header />).find("NavLink").length;
   expect(numLinks).toEqual(3);
